54739de7730b7b447edef63b67219e06ff4cab9b
Zamir Collins <collinszamir@gmail.com>
2018-08-29 21:01:17 -0500

    Updated show routes to 'portfolios#show'.

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,7 +28,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-        format.html { redirect_to portfolios_path, notice: 'The record successfullyUpdated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
